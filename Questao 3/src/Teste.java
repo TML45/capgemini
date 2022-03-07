d2860b1ea99748f780f936a7d91f821ee8a71f4c
TML45 <tassio.melo45@gmail.com>
2022-03-07 13:05:25 -0300

    Update

--- Questao 3/src/Teste.java
+++ Questao 3/src/Teste.java
@@ -8,7 +8,14 @@ class Teste {
 	void test() {
 		Encriptar sentence1 = new Encriptar("tenha um bom dia");
 		System.out.println(sentence1.encriptando());
-		assertEquals(sentence1.encriptando(),"taoaeum nmd hbi ");
+		assertEquals(sentence1.encriptando(),"taoa eum nmd hbi");
+	}
+	
+	@Test
+	void test2() {
+		Encriptar sentence2 = new Encriptar("ola mundo");
+		System.out.println(sentence2.encriptando());
+		assertEquals(sentence2.encriptando(),"omd luo an");
 	}
 
 }
