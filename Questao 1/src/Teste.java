eb0d0420f7e906f274c5a52706db97db2144c38d
TML45 <tassio.melo45@gmail.com>
2022-03-07 08:43:45 -0300

    Test update

--- Questao 1/src/Teste.java
+++ Questao 1/src/Teste.java
@@ -10,5 +10,20 @@ public class Teste {
 		System.out.println(lista1.encontrarMediana());
 		assertEquals(lista1.encontrarMediana(),4);
 	}
+	
+	@Test
+	public void test2() {
+		Mediana lista2 = new Mediana();
+		lista2.lista = new int[] {9, 2, 1, 4, 6};
+		System.out.println(lista2.encontrarMediana());
+		assertEquals(lista2.encontrarMediana(),4);
+	}
+	
+	@Test
+	public void test3() {
+		Mediana lista3 = new Mediana(new int[]{9, 2, 1, 4, 6, 0, -2});
+		System.out.println(lista3.encontrarMediana2());
+		assertEquals(lista3.encontrarMediana(),2);
+	}
 
 }
